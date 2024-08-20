958742ae8de79f1591a7e1c2d8ab78a7eaa211d7
nqoctai <2001224231@hufi.edu.vn>
2024-08-20 16:22:17 +0700

    #19

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -6,12 +6,26 @@ class MyComponent extends React.Component {
         address: 'Binh Thuan',
         age: 20
     };
+
+    handleClick = () => {
+        console.log(`Click me my button`)
+    }
+
+    handleMoveOver = () => {
+        console.log(`Move over my button`)
+    }
     // JSX
     render() {
         return (
             <div>My first Component
                 <br></br>
                 My name is: {this.state.name} and I'm from {this.state.address}
+
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleMoveOver}>Move over me</button>
+
+
+
             </div>
 
         );
