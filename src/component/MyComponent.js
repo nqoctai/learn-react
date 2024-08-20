df041158525fa6b45cd9e47b1bf9e963f8123dc2
nqoctaii <zerefmavis1210@gmail.com>
2024-08-20 23:21:29 +0700

    #22

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,51 +1,14 @@
 import React from "react";
+import UserInfor from "./Userinfor";
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'Ngoc Tai',
-        address: 'Binh Thuan',
-        age: 20
-    };
-
-    handleClick = () => {
-        console.log(`Click me my button`)
-
-        this.setState(
-            {
-                name: 'Eric',
-                age: '21'
-            }
-        )
-
-       
-    }
-
-    handleMoveOver = () => {
-        console.log(`Move over my button`)
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({name :event.target.value})
-    }
-
-    handleOnSubMit =(event) =>{
-        event.preventDefault();
-        console.log(this.state);
-    }
+   
     // JSX
     render() {
         return (
             <div>My first Component
-                <br></br>
-                My name is: {this.state.name} and I'm {this.state.age}
-
-                <button onClick={this.handleClick}>Click me</button>
-                <button onMouseOver={this.handleMoveOver}>Move over me</button>
-                <form onSubmit={(event) => {this.handleOnSubMit(event)}}>
-                    <input type="text" onChange={(event) => this.handleOnChangeInput(event)}/>
-                    <button>Submit</button>
-                </form>
-
+               
+                <UserInfor></UserInfor>
 
             </div>
 
---
+++ src/component/Userinfor.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class UserInfor extends React.Component{
+    state = {
+        name: 'Ngoc Tai',
+        address: 'Binh Thuan',
+        age: 20
+    };
+
+
+    handleOnChangeInput = (event) => {
+        this.setState({name :event.target.value})
+    }
+
+    handleOnSubMit =(event) =>{
+        event.preventDefault();
+        console.log(this.state);
+    }
+    render(){
+        return (
+            <div>
+                My name is: {this.state.name} and I'm {this.state.age}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleMoveOver}>Move over me</button>
+                <form onSubmit={(event) => {this.handleOnSubMit(event)}}>
+                <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeInput(event)}/>
+                <button>Submit</button>
+            </form>
+            </div>
+
+            
+        )
+    }
+}
+
+
+export default UserInfor;
