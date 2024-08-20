98205945eebb73d799d71925d0b380679049b8a4
nqoctaii <zerefmavis1210@gmail.com>
2024-08-20 22:39:50 +0700

    #21

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -9,21 +9,42 @@ class MyComponent extends React.Component {
 
     handleClick = () => {
         console.log(`Click me my button`)
+
+        this.setState(
+            {
+                name: 'Eric',
+                age: '21'
+            }
+        )
+
+       
     }
 
     handleMoveOver = () => {
         console.log(`Move over my button`)
+    }
+
+    handleOnChangeInput = (event) => {
+        this.setState({name :event.target.value})
+    }
+
+    handleOnSubMit =(event) =>{
+        event.preventDefault();
+        console.log(this.state);
     }
     // JSX
     render() {
         return (
             <div>My first Component
                 <br></br>
-                My name is: {this.state.name} and I'm from {this.state.address}
+                My name is: {this.state.name} and I'm {this.state.age}
 
                 <button onClick={this.handleClick}>Click me</button>
                 <button onMouseOver={this.handleMoveOver}>Move over me</button>
-
+                <form onSubmit={(event) => {this.handleOnSubMit(event)}}>
+                    <input type="text" onChange={(event) => this.handleOnChangeInput(event)}/>
+                    <button>Submit</button>
+                </form>
 
 
             </div>
