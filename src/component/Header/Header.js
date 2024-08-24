199e9e6a98035e76a071a4637130ceb12d797ba2
nqoctai <2001224231@hufi.edu.vn>
2024-08-24 22:59:33 +0700

    #53

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -20,13 +20,16 @@ const Header = () => {
 
                     </Nav>
                     <Nav>
-                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                        <button className='btn-login'>Login</button>
+                        <button className='btn-signup'>Sign up</button>
+
+                        {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
                             <NavDropdown.Item >Log in</NavDropdown.Item>
                             <NavDropdown.Item >
                                 Log out
                             </NavDropdown.Item>
                             <NavDropdown.Item >Profile</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
