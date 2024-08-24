cc0b57f1a7405793ea5bfc60f2c583767459bf39
nqoctai <2001224231@hufi.edu.vn>
2024-08-24 16:03:38 +0700

    #47

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import logo from './logo.svg';
+
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import MyComponent from './component/MyComponent';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import React from 'react';
+import Header from './component/Header/Header';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello Word
-        <MyComponent></MyComponent>
-      </div>
 
-    );
-  }
+const App = () => {
+  return (
+    <div className='app-container'>
+      <Header />
+    </div>
+  )
 }
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Hello Word with Ngoc Tai
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// }
 
 export default App;
