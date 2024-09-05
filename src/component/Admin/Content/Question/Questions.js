d2baf6d8159ce4735d8f6645083c4fbd406b0651
nqoctai <2001224231@hufi.edu.vn>
2024-09-05 10:01:45 +0700

    #96

--- src/component/Admin/Content/Question/Questions.js
+++ src/component/Admin/Content/Question/Questions.js
@@ -80,6 +80,55 @@ const Questions = (props) => {
             setQuestions(questionsClone);
         }
     }
+
+    const handleOnChange = (type, questionId, value) => {
+        if (type === 'QUESTION') {
+            let questionsClone = _.cloneDeep(questions);
+            let index = questionsClone.findIndex(item => item.id === questionId);
+            if (index > -1) {
+                questionsClone[index].description = value;
+                setQuestions(questionsClone);
+            }
+        }
+    }
+
+    const handleOnChangeFileQuestion = (questionId, event) => {
+
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1 && event.target && event.target.files && event.target.files[0]) {
+            questionsClone[index].imageFile = event.target.files[0];
+            questionsClone[index].imageName = event.target.files[0].name;
+
+            setQuestions(questionsClone);
+        }
+    }
+
+
+    const handleAnswerQuestion = (type, answerId, questionId, value) => {
+        let questionsClone = _.cloneDeep(questions);
+        let index = questionsClone.findIndex(item => item.id === questionId);
+        if (index > -1) {
+            questionsClone[index].answers = questionsClone[index].answers.map(answer => {
+                if (answer.id === answerId) {
+                    if (type === 'CHECKBOX') {
+                        answer.isCorrect = value;
+                    }
+
+                    if (type === 'INPUT') {
+                        answer.description = value;
+                    }
+                }
+                return answer;
+            })
+
+            setQuestions(questionsClone);
+        }
+    }
+
+    const handleSubmitQuestionForQuiz = () => {
+        console.log('questions', questions);
+    }
     return (
         <div className="questions-container">
             <div className='title'>
@@ -103,13 +152,19 @@ const Questions = (props) => {
                             <div key={question.id} className='q-main mb-4'>
                                 <div className='questions-content'>
                                     <div className="form-floating description">
-                                        <input type="text" className="form-control" placeholder="name@example.com" value={question.description} />
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="name@example.com"
+                                            value={question.description}
+                                            onChange={(event) => handleOnChange('QUESTION', question.id, event.target.value)}
+                                        />
                                         <label >Question {index + 1}'s Description</label>
                                     </div>
                                     <div className='group-upload'>
-                                        <label className='label-up'><RiImageAddFill></RiImageAddFill></label>
-                                        <input type='file' hidden></input>
-                                        <span>0 file is uploaded</span>
+                                        <label htmlFor={`${question.id}`} className='label-up'><RiImageAddFill></RiImageAddFill></label>
+                                        <input id={`${question.id}`} type='file' hidden onChange={(event) => handleOnChangeFileQuestion(question.id, event)}></input>
+                                        <span>{question.imageName ? question.imageName : '0 file is uploaded'}</span>
                                     </div>
                                     <div className='btn-add'>
                                         <span onClick={() => handleAddRemoveQuestion('ADD', '')}><BsFillPatchPlusFill className='icon-add'></BsFillPatchPlusFill></span>
@@ -120,9 +175,20 @@ const Questions = (props) => {
                                 {question.answers && question.answers.length > 0 && question.answers.map((answer, index) => {
                                     return (
                                         <div className='answers-content'>
-                                            <input className="form-check-input iscorrect" type="checkbox" />
+                                            <input
+                                                className="form-check-input iscorrect"
+                                                type="checkbox"
+                                                checked={answer.isCorrect}
+                                                onChange={(event) => handleAnswerQuestion('CHECKBOX', answer.id, question.id, event.target.checked)}
+                                            />
                                             <div className="form-floating answer-name">
-                                                <input type="text" className="form-control" placeholder="name@example.com" value={answer.description} />
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    placeholder="name@example.com"
+                                                    value={answer.description}
+                                                    onChange={(event) => handleAnswerQuestion('INPUT', answer.id, question.id, event.target.value)}
+                                                />
                                                 <label >Answer {index + 1}</label>
                                             </div>
 
@@ -139,6 +205,12 @@ const Questions = (props) => {
                             </div>
                         )
                     })
+                }
+                {
+                    questions && questions.length > 0 &&
+                    <div className='btn-add-question'>
+                        <button className='btn btn-warning' onClick={() => handleSubmitQuestionForQuiz()}>Save Questions</button>
+                    </div>
                 }
 
 
